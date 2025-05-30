dab3b2f1cefd3b3126577d3b057f64a78aa32433
loboramiro <104777533+loboramiro@users.noreply.github.com>
2025-05-29 22:54:45 -0700

    update cleaning, add BERT topic modeling

--- corpus_cleaning.R
+++ corpus_cleaning.R
@@ -17,8 +17,29 @@ author_map <- c(
   "Caesar, Julius" = "Caesar",
   "Mahan, A. T. (Alfred Thayer)" = "Mahan",
   "Jomini, Antoine Henri, baron de" = "Jomini",
-  "Arrian" = "Arrian"  # Not part of your project
+  "Arrian" = "Arrian" 
 )
+
+year_map <- c(
+  "Sun Tzu" = -500,
+  "Thucydides" = -400,
+  "Caesar" = -50,
+  "Clausewitz" = 1832,
+  "Jomini" = 1838,
+  "Mahan" = 1890,
+  "Arrian" = 130
+)
+
+century_map <- c(
+  "Sun Tzu" = "6th c. BCE",
+  "Thucydides" = "5th c. BCE",
+  "Caesar" = "1st c. BCE",
+  "Clausewitz" = "19th c. CE",
+  "Jomini" = "19th c. CE",
+  "Mahan" = "19th c. CE",
+  "Arrian" = "2nd c. CE"
+)
+
 
 #clean text
 df_clean <- df |>
@@ -27,9 +48,10 @@ df_clean <- df |>
     text = str_to_lower(text), 
     text = str_replace_all(text, "[[:punct:]]", ""),
     text = str_replace_all(text, "[[:digit:]]", ""),
-    author = recode(author, !!!author_map)
+    author = recode(author, !!!author_map),
+    year = year_map[author],
+    century = century_map[author]
   ) 
-  
 
 #tokenize into words
 tokens_words <- df_clean |>
@@ -38,3 +60,16 @@ tokens_words <- df_clean |>
 
 write.csv(tokens_words, "data/tokens_words_clean.csv")
 
+#create dataframe with text chunks for topic modelling
+#each chunk is five lines of text
+df_chunks <- df_clean |>
+  group_by(author, title) |>
+  mutate(line = row_number()) |>
+  group_by(author, title, chunk = (line - 1) %/% 5) |>
+  summarise(text = paste(text, collapse = " "), .groups= "drop") |>
+  mutate(
+    year = year_map[author],
+    century = century_map[author]
+  )
+  
+write.csv(df_chunks, "data/text_chunks.csv")
